2a81ff7067ec249152a83d3070fe4562a505b8b5
henry <henrytian@vip.qq.com>
2021-08-19 10:37:38 +0800

    Filter out more proxies.

--- proxy_filter.py
+++ proxy_filter.py
@@ -2,6 +2,6 @@ def fastlink(configs:tuple):
     filtered = []
     for config in configs:
         remark = config['remark']
-        if '广' in remark or '港' in remark or '日' in remark or '台' in remark:
+        if ('港' in remark or '日' in remark or '台' in remark) and config['multi'] < 3:
             filtered.append(config)
     return filtered
